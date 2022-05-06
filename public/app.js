ff22515c89a5a49d95fc1c380cfa9a399d151faa
NourNafea <nour68.nafea@gmail.com>
2022-05-06 09:07:49 +0200

    Updata FireBaseCOnfig

--- public/app.js
+++ public/app.js
@@ -1,22 +1,22 @@
 (function(){
-    var firebaseConfig = {
-        
-        apiKey: "eV6sxUs1lTPkcyQphKwf9z1u4rlOxb3nlEmMNDWS",
-          authDomain: "fire-detection-robot-default-rtdb.firebaseio.com",
-          databaseURL: "https://fire-detection-robot-default-rtdb.firebaseio.com",
-          projectId: "fire-detection-robot",
-          storageBucket: "fire-detection-robot.appspot.com",
-          messagingSenderId: "1098199709707"
-      };
-    
+  const firebaseConfig = {
+    apiKey: "AIzaSyBh0PGnJ_uNoPXLzbLpHnAQHIOkH_XpCSs",
+    authDomain: "fire-detection-robot.firebaseapp.com",
+    databaseURL: "https://fire-detection-robot-default-rtdb.firebaseio.com",
+    projectId: "fire-detection-robot",
+    storageBucket: "fire-detection-robot.appspot.com",
+    messagingSenderId: "1098199709707",
+    appId: "1:1098199709707:web:831bebbe495022f4d8ef0e",
+    measurementId: "G-NZMNZKPDR1"
+  };
     //   Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     
     var database = firebase.database(); //get reference to the database
+
     
     
-    
-    firebase.database().ref("FLAME STATE").on("value", (snapshot)=>{
+    database.ref("FLAME STATE").on("value", (snapshot)=>{
       const val = snapshot.val();
       var element1 = document.getElementById("box1");
       var element2 = document.getElementById("box2");
